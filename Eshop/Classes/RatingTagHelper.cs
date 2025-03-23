e659cf4174f1a133593e9f8617984fc46cb37c84
will <gunmonyk@gmail.com>
2025-03-23 20:08:18 +0100

    adding item browsing views

---
+++ Eshop/Classes/RatingTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Eshop.Classes
+{
+    public class RatingTagHelper : TagHelper
+    {
+        private double value = 0;
+
+        public string Value
+        {
+            get => value.ToString();
+            set => this.value = double.Parse(value);
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+
+            for(int i = 0; i < Math.Round(value); i++)
+            {
+                var builder = new TagBuilder("span");
+                builder.AddCssClass("bi bi-star-fill");
+                output.Content.AppendHtml(builder);
+            }
+
+            for(int x = 0; x < 5 - Math.Round(value); x++)
+            {
+                var builder = new TagBuilder("span");
+                builder.AddCssClass("bi bi-star");
+                output.Content.AppendHtml(builder);
+            }
+        }
+    }
+}
